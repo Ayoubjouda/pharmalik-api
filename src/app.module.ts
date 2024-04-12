f893cf90b53fc6c0e531d194d2c057d191c558af
Ayoubjouda <76652573+Ayoubjouda@users.noreply.github.com>
2024-04-12 17:46:04 +0000

    Fix:env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,13 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { PharmacyModule } from './pharmacy/pharmacy.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-const ENV = process.env.NODE_ENV;
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : `.env.${process.env.NODE_ENV}`,
     }),
     ScheduleModule.forRoot(),
     HttpModule,
